592c4c6fb31c66d7a0105191fa9a0f6c6e737742
Mehmet Berk Gürçay <mehmetgurcay@ku.edu.tr>
2018-07-01 19:15:07 +0300

    vote result sum

--- product/pages/Vote/controller.js
+++ product/pages/Vote/controller.js
@@ -1,4 +1,5 @@
 const Vote = require('./model');
+const Meal = require('./../Meal/model');
 const utility = require('./../../utility/utility.js');
 const isEmpty = utility.isEmpty;
 const respondQuery = utility.respondQuery;
@@ -6,18 +7,18 @@ const respondBadRequest = utility.respondBadRequest;
 
 exports.myVote = function(req, res, next){
     var userId = req.user._id;
-    Cart.findOne({user: userId}).populate("meal").then(function(data, err) {
+    Vote.findOne({user: userId}).populate("meal").then(function(data, err) {
         respondQuery(res,err,data,"Vote","Found")
     });
 }
 
 exports.currentResults = function(req, res, next){
-    Vote.aggregate({ $group: { _id: '$meal', total_products: { $sum: 1 } } }).exec(function(err, data) {
+    Vote.aggregate({ $group: { _id: '$meal', totalVote: { $sum: 1 } } }).exec(function(err, data) {
         // Don't forget your error handling
         // The callback with your transactions
         // Assuming you are having a Tag model
         if(!err){
-            Vote.populate(data, {path: 'meal'}, function(err, data) {
+            Meal.populate(data, {path: '_id', select: '_id name'}, function(err, data) {
                 respondQuery(res,err,data,"Result","Found")
             });
         }else{

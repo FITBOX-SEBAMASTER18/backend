5876aa01dc262eca2c314182c67b4e63095f59f6
Mehmet Berk Gürçay <mehmetgurcay@ku.edu.tr>
2018-07-01 18:21:17 +0300

    Voting Backend

---
+++ product/pages/Vote/controller.js
@@ -0,0 +1,5 @@
+const Address = require('./model');
+const utility = require('./../../utility/utility.js');
+const isEmpty = utility.isEmpty;
+const respondQuery = utility.respondQuery;
+const respondBadRequest = utility.respondBadRequest;
---
+++ product/pages/Vote/model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
+const PropertiesModel = require('./../../utility/Tools/Properties.js');
+const Properties = PropertiesModel.Properties;
+const utility = require('./../../utility/utility.js');
+const isEmpty = utility.isEmpty;
+
+const VoteSchema = new Schema({
+    meal:    {type: mongoose.SchemaTypes.ObjectId, ref: 'Meal'},
+    user:    {type: mongoose.SchemaTypes.ObjectId, ref: 'User'}
+});
+
+VoteSchema.methods.canAccess = function(user, readOnly) {
+    if(!this.properties.active())
+      return false;
+  
+    if(Auth.canAccess(this.properties, user, readOnly))
+      return true;
+  
+    return false;
+};
+  
+VoteSchema.statics.parseJSON = function (body, user) {
+
+    let properties = {};
+    if (body.readVisibility) properties.readVisibility = body.properties.readVisibility;
+    if (body.writeVisibility) properties.writeVisibility = body.properties.writeVisibility;
+
+    let object = {
+        meal: body.meal || null,
+        user: user._id || null,
+        properties: properties
+    };
+
+    if (body.properties) object.properties = properties;
+
+    object.properties.owner = user._id;
+    object = new this(object);
+    if (repOK(object))
+        return object;
+    else
+        return null;
+};
+
+const repOK = function (object) {
+    return !(isEmpty(object.user) || isEmpty(object.meal))
+};
+
+module.exports = mongoose.model('Vote', VoteSchema);
